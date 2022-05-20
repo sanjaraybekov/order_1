52af875f82caee375ba3fee55f7313a65862b9d0
Ibrohim <imirxoshimov@gmail.com>
2022-05-20 23:35:14 +0500

    fix

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import { createConnection } from "typeorm";
 import { loadBot } from "./bot";
 import { initQuestions } from "./bot/question";
+import { PASSWORD_DB } from "./config";
 // import { PASSWORD_DB } from "./config";
 import { User } from "./db/User";
 
@@ -11,7 +12,7 @@ const main = async () => {
 			database: "user_info_db",
 			username: "postgres",
 			port: 5432,
-			password: "123123",
+			password: PASSWORD_DB,
 			host: "localhost",
 			entities: [User],
 			synchronize: true,
