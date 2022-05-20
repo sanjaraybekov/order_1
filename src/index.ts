19670fa1baa55a10a461fc36895773728447723a
sanjaraybekov <sanjaraliaybekov@gmail.com>
2022-05-20 22:37:35 +0500

    asd

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,12 @@
 import { createConnection } from "typeorm";
 import { loadBot } from "./bot";
-import { PASSWORD_DB } from "./config";
 import { User } from "./db/User";
 
 const main = async () => {
 	try {
 		await createConnection({
 			type: "postgres",
-			database: "user_info_two",
+			database: "user_info_db",
 			username: "postgres",
 			port: 5432,
 			password: "123123",
